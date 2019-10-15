87324f4260ebd5b32d44341b6d38d1b48962c363
Hao(Leo) Luo <robzai@hotmail.com>
2019-10-14 21:07:54 -0700

    added PlayingCardsView and Grid no implement yet

---
+++ SetGame/PlayingCardsView.swift
@@ -0,0 +1,21 @@
+//
+//  PlayingCardsView.swift
+//  SetGame
+//
+//  Created by leo  luo on 2019-10-14.
+//  Copyright © 2019 tk.onebite. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
